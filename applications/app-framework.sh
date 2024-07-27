5bbd274c867323754c1299edad9007feca0f3253
Geoffrey Thomas <geoffrey.thomas@garmin.com>
2024-07-27 10:55:41 -0600

    Add framework specific configuration

---
+++ applications/app-framework.sh
@@ -0,0 +1,12 @@
+cd /tmp
+
+wget -O frameworkd.deb 'https://stefanhoelzl.github.io/frameworkd/frameworkd_0.2.0-1_amd64.deb'
+sudo dpkg -i ./frameworkd.deb
+rm frameworkd.deb
+
+wget -O frameworkctl.zip 'https://stefanhoelzl.github.io/frameworkd/frameworkctl@stefanhoelzl.shell-extension.zip'
+mkdir -p ~/.local/share/gnome-shell/extensions/frameworkctl@stefanhoelzl
+unzip frameworkctl.zip -d ~/.local/share/gnome-shell/extensions/frameworkctl@stefanhoelzl
+gnome-extensions enable frameworkctl@stefanhoelzl
+
+cd -
