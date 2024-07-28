b2887c072909083a9c5fe398b3c00b6b38868b8a
Geoffrey Thomas <geoff.coppertop@gmail.com>
2024-07-27 19:22:19 -0600

    Update nautilus configuration

--- applications/app-nautilus.sh
+++ applications/app-nautilus.sh
@@ -4,15 +4,6 @@ gsettings set org.gnome.nautilus.preferences default-folder-viewer "list-view"
 # Sort files by type
 gsettings set org.gnome.nautilus.preferences default-sort-order "type"
 
-# List directories first
-gsettings set org.gnome.nautilus.preferences sort-directories-first true
-
-# Never show image thumbnails
-gsettings set org.gnome.nautilus.preferences show-image-thumbnails "never"
-
-# Use tree-view
-gsettings set org.gnome.nautilus.list-view use-tree-view true
-
 # Small zoom level
 gsettings set org.gnome.nautilus.list-view default-zoom-level "small"
 
