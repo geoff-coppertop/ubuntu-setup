53b4cd550fcccbcf866e6ca5a182d3e1a068268b
Geoffrey Thomas <geoff.coppertop@gmail.com>
2024-07-30 20:15:45 -0600

    Remove flameshot .desktop file

--- applications/app-flameshot.sh
+++ applications/app-flameshot.sh
@@ -1,2 +1,5 @@
 # Flameshot is a nice step-up over the default Gnome screenshot tool
 flatpak install -y flathub org.flameshot.Flameshot
+
+# App doesn't do anything when started from the app grid
+sudo rm -rf /var/lib/flatpak/exports/share/applications/org.flameshot.Flameshot.desktop
