9d00faf67fc5ef6952be2b3e7cc33068539e5858
Geoffrey Thomas <geoff.coppertop@gmail.com>
2024-07-30 20:13:36 -0600

    Add flatseal to flatpak configuration

--- prerequisites/1_flatpak.sh
+++ prerequisites/1_flatpak.sh
@@ -1,3 +1,5 @@
 sudo apt-get install -y flatpak
 sudo apt-get install -y gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo 1>/dev/null
+
+flatpak install -y flathub com.github.tchx84.Flatseal
